b160bfdaa4c227ad276e5f29e538cf2b7ab43b95
jesusfb <jfigueroa.beltran@gmail.com>
2024-05-01 15:56:46 -0700

    Update server.py

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 from sqlalchemy import create_engine
 from json import dumps
+from collections.abc import Mapping
 
 db_connect = create_engine('sqlite:///chinook.db')
 app = Flask(__name__)
